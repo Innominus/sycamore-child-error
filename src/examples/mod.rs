79786387d7db450aeb7a5f45df22aebc789ee3ec
Michael Kadziela <michael.kadziela@outlook.com>
2022-09-30 17:28:51 +1000

    created failing scenario

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,7 +1,1 @@
-mod reactive_other_component;
-pub(crate) use reactive_other_component::OtherComponent;
-mod fetch_in_component;
-mod sub_component;
-pub(crate) use sub_component::{DrawerOverlay, TitleBar};
-
-pub(crate) use fetch_in_component::UpdateFetchInComponent;
+pub mod sub_component;
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,28 @@
+use examples::sub_component::{DrawerOverlay, TitleBar};
 use sycamore::prelude::*;
-use sycamore::suspense::Suspense;
 
 mod examples;
 
 #[component]
-async fn App<G: Html>(cx: Scope<'_>) -> View<G> {
-    let input = create_signal(cx, String::new());
-
+fn App<G: Html>(cx: Scope<'_>) -> View<G> {
     view! { cx,
-        // Example Other component reactive
-        h1 {"Reactive component"}
-        p {input(bind:value=input, type="text", placeholder="Type here")}
-        examples::OtherComponent(input=input)
-
-        hr {}
-
-        // Example fetch new joke and show the joke
-        h1 {"Fetch on button action"}
-        examples::UpdateFetchInComponent
-
-        hr {}
-
         // Component with sub item
         h1 {"Sub component"}
-        examples::DrawerOverlay {
+        DrawerOverlay() {
             div(class="w-full h-full flex flex-col items-center") {
-                examples::TitleBar
+                TitleBar
                 div {
                     "Home!"
                 }
             }
         }
-
     }
 }
 
 fn main() {
     sycamore::render(|cx| {
         view! { cx,
-            Suspense(fallback=view! { cx, "Loading any async event..." }) {
-                App {}
-            }
+            App {}
         }
     });
 }
